35ba23914f7317ba969075f69548854104dccfa9
frederikbroth <frboe17@student.sdu.dk>
2019-11-14 00:15:08 +0100

    Added navbar animation
    Also installed:
    react-animations
    styled-components
    
    This is used for other animations

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,19 +2,32 @@ import React, { Component, useState } from "react";
 import Navbar from "./Navbar";
 import menuLogo from "../img/menu.svg";
 
+// import styled, { keyframes } from "styled-components";
+// import { fadeIn} from "react-animations";
+
+// const fadeInUpAni = keyframes`${fadeIn}`;
+
+// const FadeinUpAni = styled.div`
+// 	animation: 0.5s ${fadeInUpAni};
+// `;
 function Footer() {
 	const [navbarOpen, setNavbarOpen] = useState(false);
 
 	function handleClick() {
 		setNavbarOpen(navbarOpen ? false : true);
+		const navbar = document.getElementById("navbar-popup");
+		navbar.classList.toggle("open-nav");
 	}
 
 	return (
-		<footer>
-			<img src={menuLogo} alt="menuicon" onClick={handleClick} />
-			<label>Menu</label>
-			{navbarOpen ? <Navbar /> : null}
-		</footer>
+		<div>
+			<Navbar />
+			<footer>
+				<img src={menuLogo} alt="menuicon" onClick={handleClick} />
+
+				<label>Menu</label>
+			</footer>
+		</div>
 	);
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 function Navbar() {
 	return (
-		<div className="navbar-popup">
+		<div className="navbar-popup" id="navbar-popup">
 			<ul>
 				<li>place1</li>
 				<li>place2</li>
